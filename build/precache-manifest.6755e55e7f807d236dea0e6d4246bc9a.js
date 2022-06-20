97cbfc1edb0f0f96b11c0ed929154ed47c0dcb70
L3monPL <mikolaj.andrzejewski999@gmail.com>
2022-06-20 16:10:13 +0200

    https://github.com/L3monPL/ProjectReact

---
+++ build/precache-manifest.6755e55e7f807d236dea0e6d4246bc9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d9a4944aedf1c9b5059ac2d7d9dd75",
+    "url": "/ProjectReact/index.html"
+  },
+  {
+    "revision": "cb1f39627c342c5a83de",
+    "url": "/ProjectReact/static/css/main.4f7e161c.chunk.css"
+  },
+  {
+    "revision": "81f815ed5fd7a8b4aae6",
+    "url": "/ProjectReact/static/js/2.bb7293a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ProjectReact/static/js/2.bb7293a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1f39627c342c5a83de",
+    "url": "/ProjectReact/static/js/main.aff3710e.chunk.js"
+  },
+  {
+    "revision": "9b9aab2267ef560cb52f",
+    "url": "/ProjectReact/static/js/runtime-main.859de3da.js"
+  },
+  {
+    "revision": "26ead125d728dc1c913e17fd3fd080eb",
+    "url": "/ProjectReact/static/media/CV.26ead125.pdf"
+  },
+  {
+    "revision": "803939008908bbf6aed3684b8ff09ce3",
+    "url": "/ProjectReact/static/media/imageProf.80393900.jpg"
+  }
+]);
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import './portfolio.css'
 
+
+const data = [
+  {
+    id: 1,
+    image: 'https://img.freepik.com/darmowe-wektory/recznie-rysowane-tworcow-stron-internetowych_23-2148819604.jpg?w=2000',
+    title: 'Strona pierwsza',
+    github: 'https://websiters.pl/',
+    demo: 'https://websiters.pl/'
+  },
+  {
+    id: 2,
+    image: 'https://media.kasperskydaily.com/wp-content/uploads/sites/95/2021/02/02203039/deep-web-dark-web-darknet-surface-web-difference-featured.jpg',
+    title: 'Strona druga',
+    github: 'www.google.pl',
+    demo: 'www.websiters.pl'
+  },
+];
+
+// const data2 = data.filter(e => e.id > 1);
+// console.log(data2);
+
 export const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -10,16 +31,22 @@ export const Portfolio = () => {
       </div>
 
       <div className="container_0 portfolio_container">
-        <article className='portfolio_item'>
-            <div className="portfolio_item_image">
-                <img src="https://img.freepik.com/darmowe-wektory/recznie-rysowane-tworcow-stron-internetowych_23-2148819604.jpg?w=2000" alt="" />
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <div className="portfolio_item_cta">
-                <a href="https://gitbub.com" className='btn_portfolio' target='_blank'>GitHub</a>
-                <a href="https://gitbub.com" className='btn_portfolio btn-2_portfolio' target='_blank'>Live Demo</a>
-            </div>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) =>{
+            return(
+              <article key={id} className='portfolio_item'>
+                  <div className="portfolio_item_image">
+                      <img src={image} alt={title} />
+                  </div>
+                  <h3>{title}</h3>
+                  <div className="portfolio_item_cta">
+                      <a href={github} className='btn_portfolio' target='_blank'>GitHub</a>
+                      <a href={demo} className='btn_portfolio btn-2_portfolio' target='_blank'>Live Demo</a>
+                  </div>
+              </article>
+              )
+            })
+        }
       </div>
     </section>
   )
